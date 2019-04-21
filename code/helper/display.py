ab516d497f5912118979bdb0bf72d1ee0b29b050
Amelia G. Meyer <agmlego@gmail.com>
2019-04-21 03:18:33 -0400

    Timer for screen blanking to save OLED

--- code/helper/display.py
+++ code/helper/display.py
@@ -5,13 +5,22 @@ from luma.oled.device import ssd1322
 
 from PIL import Image,ImageFont
 
+import signal
+
 class Display:
     def __init__(self):
         self._serial = spi(device=0, port=0, bus_speed_hz=32000000)
         self._device = ssd1322(self._serial,width=256,height=64)
         self._fnt = ImageFont.truetype('/home/agmlego/src/b612/fonts/ttf/B612Mono-Regular.ttf',16)
+        signal.signal(signal.SIGALRM,self.handler)
+
+    def handler(self,signum, frame):
+        print('Time!')
+        with canvas(self._device) as draw:
+            draw.rectangle(self._device.bounding_box, outline='black', fill='black')
 
     def splash(self,slot=''):
+        signal.alarm(60)
         with canvas(self._device) as draw:
             draw.rectangle(self._device.bounding_box, outline='white', fill='black')
             draw.text((2,2),'Welcome to i3Detroit!\nPlease make a selection: \n%s'%slot,font=self._fnt,fill='white')
